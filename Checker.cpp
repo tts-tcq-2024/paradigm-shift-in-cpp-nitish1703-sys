e5d32a44497c17bd2eb9fcd164234383bfd41586
nitish1703-sys <nitish.tanguturi@gmail.com>
2024-06-26 14:35:35 +0530

    fixing the build issue

--- Checker.cpp
+++ Checker.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0.00 || temperature > 45.00) {
+  if(temperature < 0 || temperature > 45) {
     cout << "Temperature out of range!\n";
     return false;
-  } else if(soc < 20.00 || soc > 80.00) {
+  }
+  
+  if(soc < 20 || soc > 80) {
     cout << "State of Charge out of range!\n";
     return false;
-  } else if(chargeRate > 0.8) {
+  }
+  if(chargeRate > 0.8) {
     cout << "Charge Rate out of range!\n";
     return false;
   }
