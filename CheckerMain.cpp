2fcb125bb78234a5dc4aa14f7ba4d0c3fb4da84a
nitish1703-sys <nitish.tanguturi@gmail.com>
2024-07-24 14:43:13 +0530

    CheckerMain.cpp

---
+++ CheckerMain.cpp
@@ -0,0 +1,14 @@
+#include "Checksforbattery.hpp"
+
+int main()
+{
+  BMS::BatteryManagementSystem BMS;
+  assert(BMS.batteryIsOk(25, 70, 0.7) == true);
+  assert(BMS.batteryIsOk(50, 85, 0) == false);
+  assert(BMS.batteryIsOk(30, 70, 0.1) == false);
+  assert(BMS.batteryIsOk(30, 70, 0.9) == false);
+  assert(BMS.batteryIsOk(1, 70, 0.7) == true);
+  assert(BMS.batteryIsOk(25, 78, 0.7) == true);
+  assert(BMS.batteryIsOk(15, 50, 0.79) == true);
+  return 0;
+}
